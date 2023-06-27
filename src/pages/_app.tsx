9cdb50c130ad4d036667066fe9d3d672bfc0a4d6
kokoro-hart <tobita@wd-flat.com>
2023-06-27 14:33:41 +0900

    feat: storage commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,26 @@
+// TODO: あとて↓のコメントアウトを外してChakraBaseProviderを使ってみる
+
+// `@chakra-ui/theme` is a part of the base install with `@chakra-ui/react`
+// import chakraTheme from "@chakra-ui/theme";
+
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import "ress";
-
 import "@/styles/globals.css";
 
+// const { Button } = chakraTheme.components;
+// const theme = extendBaseTheme({
+//   components: {
+//     Button,
+//   },
+// });
+
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default App;
